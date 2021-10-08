16540172eca5bb2886a6dc6bb8f3274a7e1f3811
sam <sd@sams-iMac.local>
2021-10-07 17:26:55 -0700

    initial commit

---
+++ src/main.js
@@ -0,0 +1,39 @@
+
+import * as THREE from 'three';
+import css from './css/style.css';
+
+export default class Main
+{
+  constructor()
+  {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( this.renderer.domElement );
+
+    this.geometry = new THREE.BoxGeometry();
+    this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    this.cube = new THREE.Mesh( this.geometry, this.material );
+    this.scene.add( this.cube );
+
+    this.camera.position.z = 5;
+
+    this.animate = this.animate.bind(this);
+
+    this.animate();
+  }
+
+  animate(){
+    requestAnimationFrame( this.animate );
+
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+
+    this.renderer.render( this.scene, this.camera );
+  };
+
+}
+
+new Main();
